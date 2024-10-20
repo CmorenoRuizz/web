ab4e2ff35a613665faa0606cdce262baf8c43da1
Carlos <camoru@gmail.com>
2024-10-20 19:10:27 +0200

    final octubre

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/home', function(req, res, next) {
+  res.redirect("/home.html");
+});
+
+router.get('/login', function(req, res, next) {
+  res.redirect("/login.html");
+});
+
+router.get('/contacto', function(req, res, next) {
+  res.redirect("/contacto.html");
+});
+
+
 module.exports = router;
